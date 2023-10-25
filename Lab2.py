e8e8fcb296cf0caaeba8df9624caa24451c55225
Amos <amosong.22@ichat.sp.edu.sg>
2023-10-26 00:39:31 +0800

    bmi classification

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,13 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def calculate_bmi(height, weight):
+ print("Height = " + str(height))
+ print("Weight = " + str(weight))
+ bmi= weight/pow(height,2)
+ print("BMI =" + str(bmi))
+ if(bmi<18.5):
+     print("You are underweight")
+ elif(bmi<=25):
+     print("You are normal weight")
+ elif(bmi>25):
+     print("You are overweight")
+
+calculate_bmi(weight=18.4, height=1)
