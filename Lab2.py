962b335ae4fdb06a5f8070a53c1e705ce00b6209
Amos <amosong.22@ichat.sp.edu.sg>
2023-10-26 01:30:14 +0800

    v1.1

--- Lab2.py
+++ Lab2.py
@@ -1,13 +1,44 @@
-def calculate_bmi(height, weight):
- print("Height = " + str(height))
- print("Weight = " + str(weight))
- bmi= weight/pow(height,2)
- print("BMI =" + str(bmi))
- if(bmi<18.5):
-     print("You are underweight")
- elif(bmi<=25):
-     print("You are normal weight")
- elif(bmi>25):
-     print("You are overweight")
+def calc_average(list):
+ total=0
+ for i in range(len(list)):
+  total+=list[i]
+ avg=total/len(list)
+ return avg
 
-calculate_bmi(weight=18.4, height=1)
+def get_user_input():
+ numbers=input()
+ numberlist=numbers.split(",")
+ for i in range(len(numberlist)):
+  numberlist[i]=float(numberlist[i])
+ return numberlist
+
+def find_min_max(list):
+ min=list[0]
+ max=0
+ for i in range(len(list)):
+  if (list[i]<min):
+   min=list[i]
+  if(list[i]>max):
+   max=list[i]
+ minmaxlist=[min,max]
+ return minmaxlist
+
+def sort_temperature(list):
+ return list
+
+def calc_median_temperature(list):
+ return list
+
+def display_main_menu():
+ print("Enter some numbers separated by commas (e.g. 5, 67,32)")
+ return
+def main():
+ print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+ display_main_menu()
+ num_list = get_user_input()
+
+ print(calc_average(num_list))
+ print(find_min_max(num_list))
+
+if __name__ == "__main__":
+     main()
